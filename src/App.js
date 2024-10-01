d5453a8364cab0b2aeed964659cd6b26b380ba80
ehdtjs0612 <inko51366@naver.com>
2024-10-01 17:59:49 +0900

    second: get movie lists usting public movie api

--- src/App.js
+++ src/App.js
@@ -2,31 +2,40 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const apiSrc = "https://api.coinpaprika.com/v1/tickers";
-  const [coins, setCoins] = useState([]);
+  const url =
+    "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year";
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const res = await fetch(url);
+    const movie = await res.json();
+
+    setMovies(movie.data.movies);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    fetch(apiSrc)
-      .then((res) => res.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
 
   return (
     <div>
-      <h1>the coins {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select>
-          {coins.map((coin, i) => (
-            <option>
-              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image}></img>
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((e, i) => (
+                  <li key={i}>{e}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
     </div>
   );
